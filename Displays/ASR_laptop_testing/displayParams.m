ee8757507efc1210c194445d2fb8f8995acbe64e
arokem <arokem@berkeley.edu>
2009-10-26 09:50:28 -0700

    Added a Displays directory, with ASR_laptop_testing, as an example

---
+++ Displays/ASR_laptop_testing/displayParams.m
@@ -0,0 +1,16 @@
+function params = displayParams
+
+% Critical parameters
+params.numPixels = [800 600];
+params.dimensions = [16 12];
+params.distance = 55; %cm
+params.frameRate = 60;
+params.cmapDepth = 8;
+params.screenNumber = 0;       
+params.thisRect = [0 0 params.numPixels];
+
+% Descriptive parameters
+params.computerName = 'ariel_macbook';
+params.monitor = 'built-in LCD';
+params.card = 'GeForce8600'; 
+params.position = 'Wherever the laptop happens to be :)';
